8c94584b7c2c8b106f2ad5f1efa0973dda492624
Wav3F0rm <tjmoggie@student.christscollege.com>
2023-08-10 10:32:54 +1200

    Added Bluetooth Connectivity
    Used simple BT Serial library to add bluetooth connectivity to the app. This allowed it to connect to a device and receive and react to information

--- appDevelopment/TJMCycleComputer/app/src/main/java/com/TJM/cyclecomputer/MainActivity.java
+++ appDevelopment/TJMCycleComputer/app/src/main/java/com/TJM/cyclecomputer/MainActivity.java
@@ -1,19 +1,36 @@
 package com.TJM.cyclecomputer;
 
+
 import static java.lang.Boolean.TRUE;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 // import needed items from spotify app remote, allows user to connect
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
 
+// import android bluetooth serial library
+import com.harrysoft.androidbluetoothserial.SimpleBluetoothDeviceInterface;
+import com.harrysoft.androidbluetoothserial.BluetoothManager;
+import com.harrysoft.androidbluetoothserial.BluetoothSerialDevice;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.pm.PackageManager;
 import android.util.Log;  // import log functionality for debugging
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Collection;
+import java.util.Objects;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,11 +38,15 @@ public class MainActivity extends AppCompatActivity {
     private static final String REDIRECT_URI = "com.TJM.cyclecomputer://callback";
     private SpotifyAppRemote mSpotifyAppRemote;
 
-    public boolean isPaused;
+    boolean isPaused;
     public String songName;
     public String artistName;
     public long timeElapsed;
     public long totalTimeMillis;
+
+    String BTName;
+    String BTmac;
+    private final CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,10 +86,39 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
+    @Override
+    protected void onStop () {
+        super.onStop();
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+    }
+
+    // When Play/Pause is clicked
+    public void onToggleClick (View view){
+        if (isPaused == TRUE) {
+            mSpotifyAppRemote.getPlayerApi().resume();
+        } else {
+            mSpotifyAppRemote.getPlayerApi().pause();
+        }
+    }
+
+    // When Skip forward is clicked
+    public void onSkipClick (View view){
+        mSpotifyAppRemote.getPlayerApi().skipNext();
+    }
+
+    // When Skip backward is clicked
+    public void onRewindClick (View view){
+        mSpotifyAppRemote.getPlayerApi().skipPrevious();
+    }
+
+    // When connected to Bluetooth is Clicked
+    public void onConnectBluetoothClick (View view) {
+            setupBT();
+    }
+
+    // When Spotify Player is Connected
     private void connected() {
         // Code Here
-        //mSpotifyAppRemote.getPlayerApi().play("spotify:track:6nFYXpBgrNcZjbtNEuc6yR");
-
         // Subscribe to PlayerState
         mSpotifyAppRemote.getPlayerApi()
                 .subscribeToPlayerState()
@@ -82,28 +132,7 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
-    @Override
-    protected void onStop () {
-        super.onStop();
-        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
-    }
-
-    public void onToggleClick (View view){
-        if (isPaused == TRUE) {
-            mSpotifyAppRemote.getPlayerApi().resume();
-        } else {
-            mSpotifyAppRemote.getPlayerApi().pause();
-        }
-    }
-
-    public void onSkipClick (View view){
-        mSpotifyAppRemote.getPlayerApi().skipNext();
-    }
-
-    public void onRewindClick (View view){
-        mSpotifyAppRemote.getPlayerApi().skipPrevious();
-    }
-
+    // Update Metadata displayed in app
     public void updateMetadata() {
         long minutes = (totalTimeMillis / 1000)  / 60;
         int seconds = (int)((totalTimeMillis / 1000) % 60);
@@ -120,5 +149,92 @@ public class MainActivity extends AppCompatActivity {
 
         TextView dispTotalTime = findViewById(R.id.totalTime);
         dispTotalTime.setText(totalTime);
+    }
+
+    //
+    public void setupBT() {
+        // Check for app Bluetooth Permission
+        String[] requestConnectPermission = {android.Manifest.permission.BLUETOOTH_CONNECT};
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, requestConnectPermission, 1);
+            return;
+        }
+        String[] requestScanPermission = {android.Manifest.permission.BLUETOOTH_SCAN};
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, requestScanPermission, 2);
+            return;
+        }
+
+        // Setup Bluetooth Manager
+        BluetoothManager bluetoothManager = BluetoothManager.getInstance();
+        boolean bluetoothAvailable = getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH);
+        if (!bluetoothAvailable) {
+            // Bluetooth is not available on this mobile device, tell the user
+            Toast.makeText(this, "Bluetooth not available on this device.", Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+        // show list of paired bluetooth devices
+        try {
+            Collection<BluetoothDevice> pairedDevices = bluetoothManager.getPairedDevicesList();
+            for (BluetoothDevice device : pairedDevices) {
+                Log.d("My Bluetooth App", device.getName());
+                Log.d("My Bluetooth App", device.getAddress());
+
+                if (Objects.equals(device.getName(), "ESP32testAA")) {
+                    Log.d("My Bluetooth App", "Device Paired! :0");
+                    BTName = device.getName();
+                    BTmac = device.getAddress();
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // Handle Error
+            Toast.makeText(this, "No Bluetooth Devices Paired", Toast.LENGTH_LONG).show();
+            Log.d("My Bluetooth App", "Error: " + e);
+        }
+        // Call method to connect
+        compositeDisposable.add(bluetoothManager.openSerialDevice(BTmac)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::onBTConnected, this::onBTError));
+    }
+
+    private void onBTConnected(BluetoothSerialDevice connectedDevice) {
+        // Code
+        SimpleBluetoothDeviceInterface deviceInterface = connectedDevice.toSimpleDeviceInterface();  // Keep instance of device
+        deviceInterface.setListeners(this::onBTMessageReceived, this::onBTMessageSent, this::onBTError);  // Listen to bluetooth events
+        Log.d("My Bluetooth App", "Successfully Connected");
+        deviceInterface.sendMessage("Successfully Connected!");  // Send confirmation message
+
+        TextView deviceName = findViewById(R.id.deviceName);
+        TextView deviceMac = findViewById(R.id.deviceMac);
+        deviceName.setText("Device Name: " + BTName);
+        deviceMac.setText("Device Address: " + BTmac);
+    }
+
+    private void onBTMessageReceived(String message) {
+        Toast.makeText(this, "Received a message! Message was: " + message, Toast.LENGTH_LONG).show();
+
+        if(Objects.equals(message, "Toggle")){
+            if (isPaused == TRUE) {
+                mSpotifyAppRemote.getPlayerApi().resume();
+            } else {
+                mSpotifyAppRemote.getPlayerApi().pause();
+            }
+        } else if (Objects.equals(message, "Skip Next")){
+            mSpotifyAppRemote.getPlayerApi().skipNext();
+        } else if (Objects.equals(message, "Skip Previous")) {
+            mSpotifyAppRemote.getPlayerApi().skipPrevious();
+        }
+    }
+
+    private void onBTMessageSent(String message) {
+        Toast.makeText(this, "Sent a message! Message was: " + message, Toast.LENGTH_LONG).show();
+    }
+
+    private void onBTError(Throwable error){
+        // Code
+        Log.d("My Bluetooth App", "error: " + error);
     }
 }
---
+++ appDevelopment/Testing/app/src/main/java/com/tjm/testapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tjm.testapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.harrysoft.
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
